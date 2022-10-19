10462a85e509564c8daa1eb29cb521e6000a19f3
wbryon <hatami1974@yandex.ru>
2022-10-19 19:16:12 +0300

    класс ReadData перенесён в класс MonthlyReport. Значения полей обработаны и переданы в мапу

--- src/MonthlyReport.java
+++ src/MonthlyReport.java
@@ -6,17 +6,30 @@ import java.util.*;
  * Класс, предоставляющий месячный отчёт, содержащий данные о доходах и расходах в рамках одного календарного месяца
  */
 public class MonthlyReport {
-    ReadData readData = new ReadData();
-    List<String> itemName = new ArrayList<>();
-    List<Boolean> isExpense = new ArrayList<>();
-    List<Integer> quantity = new ArrayList<>();
-    List<Integer> sumOfOne = new ArrayList<>();
-    List<List<String>> monthData = readFileContentsOrNull(getPathToMonthlyReports());
-    Map<Integer, MonthlyReport> monthlyReport = new HashMap<>();
+    ReadMonthData januaryReport = new ReadMonthData("resources/m.202101.csv");
+    ReadMonthData februaryReport = new ReadMonthData("resources/m.202102.csv");
+    ReadMonthData marchReport = new ReadMonthData("resources/m.202103.csv");
+    Map<Integer, ReadMonthData> monthlyReport = new HashMap<>();
     public MonthlyReport() {
+        monthlyReport.put(0, januaryReport);
+        monthlyReport.put(1, februaryReport);
+        monthlyReport.put(2, marchReport);
+    }
+    public void getMostProfitableProduct() {}
+    public void getBiggestWaste() {}
 
-        for (int i = 0; i < monthData.size(); i++) {
-            for (String line : monthData.get(i).subList(1, monthData.get(i).size())) {
+    private class ReadMonthData {
+        public ReadMonthData(String source) {
+            parseLinesFromFile(readFileContentsOrNull(source));
+        }
+
+        private List<String> itemName = new ArrayList<>();
+        private List<Boolean> isExpense = new ArrayList<>();
+        private List<Integer> quantity = new ArrayList<>();
+        private List<Integer> sumOfOne = new ArrayList<>();
+        private void parseLinesFromFile(List<String> file) {
+
+            for (String line : file.subList(1, file.size())) {
                 String[] lines = line.split(",");
                 itemName.add(lines[0]);
                 isExpense.add(Boolean.parseBoolean(lines[1]));
@@ -24,43 +37,36 @@ public class MonthlyReport {
                 sumOfOne.add(Integer.parseInt(lines[3]));
             }
         }
-    }
-    public void getMostProfitableProduct(HashMap<String, List<String>> monthlyReport) {}
-    public void getBiggestWaste() {}
-
-    private List<List<String>> readFileContentsOrNull(ArrayList<String> source) { // Метод, считывающий содержимое месячного отчёта
-        List<List<String>> monthReports = new ArrayList<>();
-        try {
-            for (String path : source) {
-                monthReports.add(Arrays.asList(Files.readString(Path.of(path)).split("\r?\n")));
-            }
-            return monthReports;
-        } catch (IOException e) {
-            System.out.println("Невозможно прочитать файл с месячным отчётом. Возможно, файл не находится в нужной директории.");
-            return null;
-        }
-    }
-
-    class MonthData {
-        List<List<String>> itemName = new ArrayList<>();
-        List<List<Boolean>> isExpense = new ArrayList<>();
-        List<List<Integer>> quantity = new ArrayList<>();
-        List<List<Integer>> sumOfOne = new ArrayList<>();
-    }
-    private ArrayList<String> getPathToMonthlyReports() {
-        ArrayList<String> pathToMonthlyReports = new ArrayList<>();
-        Path path = Path.of("resources");
-        try (DirectoryStream<Path> files = Files.newDirectoryStream(path)) {
-            for (Path fileName : files) {
-                if (fileName.toString().startsWith("resources/m")) {
-                    pathToMonthlyReports.add(fileName.toString());
+        private List<String> readFileContentsOrNull(String source) { // Метод, считывающий содержимое месячного отчёта
+            List<String> currentMonthReport = new ArrayList<>();
+            try {
+                String[] lines = Files.readString(Path.of(source)).split("\r?\n");
+                for (String line : lines) {
+                    currentMonthReport.add(line);
                 }
+                return currentMonthReport;
+            } catch (IOException e) {
+                System.out.println("Невозможно прочитать файл с месячным отчётом. Возможно, файл не находится в нужной директории.");
+                return null;
             }
-            Collections.sort(pathToMonthlyReports);
-            return pathToMonthlyReports;
-        } catch (IOException e) {
-            System.out.println("Что-то пошло не так");
-            return null;
         }
+
+        private ArrayList<String> getPathToMonthlyReports() {
+            ArrayList<String> pathToMonthlyReports = new ArrayList<>();
+            Path path = Path.of("resources");
+            try (DirectoryStream<Path> files = Files.newDirectoryStream(path)) {
+                for (Path fileName : files) {
+                    if (fileName.toString().startsWith("resources/m")) {
+                        pathToMonthlyReports.add(fileName.toString());
+                    }
+                }
+                Collections.sort(pathToMonthlyReports);
+                return pathToMonthlyReports;
+            } catch (IOException e) {
+                System.out.println("Что-то пошло не так");
+                return null;
+            }
+        }
+
     }
 }
